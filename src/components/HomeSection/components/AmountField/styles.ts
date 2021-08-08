4d2a988fe558a09e09c04d245deaba9354cff5b7
Thiago Moraes <user000.000resu@gmail.com>
2021-08-08 20:37:24 -0300

    Unfound icon on AmountField

--- src/components/HomeSection/components/AmountField/styles.ts
+++ src/components/HomeSection/components/AmountField/styles.ts
@@ -3,7 +3,6 @@ import styled from "styled-components";
 export const Container = styled.div<{ visibility: string }>`
 	display: flex;
 	align-items: center;
-	gap: 1rem;
 	color: var(--secondary);
 	border-bottom: 1.5px solid var(--secondary); 
 	width: 16rem;
@@ -11,31 +10,47 @@ export const Container = styled.div<{ visibility: string }>`
 	cursor: pointer;
 	transition: filter 0.3s;
 
+	
 	div {
 		display: flex;
-		align-items: flex-start;
-		flex-direction: column;
+		align-items: center;
 		gap: 0.25rem;
-
-		h4 {
-			font-size: ${({ visibility }) => visibility === "false" ? "1.2rem" : "0.75rem"};
-			color: ${({ visibility }) => visibility === "false" && "var(--primary)"};
+		
+		> svg {
+			margin: auto 0.5rem;
 		}
 
-		p {
-			color: var(--green);
-			font-weight: 600;
-			font-size: 1.2rem;
-			display: ${({ visibility }) => visibility === "false" && "none"};
+		span {
+			display: flex;
+			align-items: flex-start;
+			justify-content: flex-start;
+			flex-direction: column;
+			gap: 0.25rem;
+	
+			h4 {
+				font-size: ${({ visibility }) => visibility === "false" ? "1.2rem" : "0.75rem"};
+				color: ${({ visibility }) => visibility === "false" && "var(--primary)"};
+			}
+	
+			p {
+				color: var(--green);
+				font-weight: 600;
+				font-size: 1.2rem;
+				display: ${({ visibility }) => visibility === "false" && "none"};
+			}
 		}
 	}
 
 
-	& > svg {
-		margin-right: 0.5rem;
-		margin-left: auto;
+	svg {
 		font-size: 1.4rem;
+	
+		&:last-child {
+			margin-right: 0.5rem;
+			margin-left: auto;
+		}
 	}
+
 
 	&:hover {
 		filter: brightness(0.8);
